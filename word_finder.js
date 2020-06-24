e1785c37f6ee739050c862a5188a8ce8ecdb268c
AsherSeiling <f14asher@gmail.com>
2020-06-24 13:42:03 -0500

    Inital commit

---
+++ word_finder.js
@@ -0,0 +1,14 @@
+// 2020 Seiling Enterprises
+
+let parsed_words = [];
+
+function main(){
+    var words = document.getElementById("sentance").value;
+    console.log(words);
+}
+
+function parse_words(){
+    for(let char in words){
+        
+    };
+}
