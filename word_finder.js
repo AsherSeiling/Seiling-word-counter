26723609a157e98ed95cf93b397f770afcc1857a
AsherSeiling <f14asher@gmail.com>
2020-06-24 16:38:58 -0500

    Finished word to list parser and counter need to  add frontend.

--- word_finder.js
+++ word_finder.js
@@ -1,14 +1,57 @@
 // 2020 Seiling Enterprises
 
-let parsed_words = [];
+var parsed_words = [];
+
+var num_occurr = 0;
+
+var buffer = "";
+
+var words;
+var words_find;
 
 function main(){
-    var words = document.getElementById("sentance").value;
-    console.log(words);
+    words = document.getElementById("sentance").value;
+    word_find = document.getElementById("search_word").value;
+    let num = 0;
+    let char = "";
+    for (char in words){
+        if (words.charAt(num) == " "){
+            parsed_words.push(buffer);
+            buffer = "";
+            console.log("space");
+        }
+        else if (words.charAt(num) == "."){
+            if (words[num + 1] != " "){
+                parsed_words.push(toString(buffer));
+                buffer = "";
+            }
+        }
+        else if (words.charAt(num) == ","){
+            parsed_words.push(buffer);
+            buffer = "";
+        }
+        else if (words.charAt(num) == ";"){
+            parsed_words.push(buffer);
+            buffer = "";
+        }
+        else if (words.charAt(num) == ":"){
+            parsed_words.push(buffer);
+            buffer = "";
+        }
+        else {
+            buffer += words.charAt(num);
+        };
+        num += 1;
+    }
+
+    for (let chars in parsed_words){
+        if (parsed_words[chars] == word_find){
+            num_occurr += 1;
+            console.log(num_occurr);
+        }
+    }
 }
 
-function parse_words(){
-    for(let char in words){
-        
-    };
+function place_data(){
+
 }
